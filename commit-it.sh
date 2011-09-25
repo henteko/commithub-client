b9d6f64de5167996a57f390f6343df0d8dfe41b6
Atsuya Takagi <atsuya.takagi@gmail.com>
2011-09-24 19:33:11 -0700

    adding config file.

--- commit-it.sh
+++ commit-it.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
+URL=`cat config`
 REPOSITORY=`git config --get remote.origin.url`
 COMMIT_INFO=`git log -1 --pretty=oneline HEAD`
-FILE_NAME="${HOME}/.commithub/scratch/"`date "+%s"`".jpg"
+FILE_NAME=`date "+%s"`".jpg"
 
-echo ${REPOSITORY}
-echo ${COMMIT_INFO}
-echo ${FILE_NAME}
+echo ${URL}
 
 cd ${HOME}/.commithub/scratch
-pwd
-
 imagesnap ${FILE_NAME} > /dev/null
-curl -silent -o /dev/null http://localhost:3000/upload -F "picture=@${FILE_NAME};commit=${COMMIT_INFO};repo=${REPOSITORY}"
-
-echo ${FILE_NAME}
+curl -silent -o /dev/null ${URL} -F "picture=@${FILE_NAME};commit=${COMMIT_INFO};repo=${REPOSITORY}"
 rm ${FILE_NAME}
