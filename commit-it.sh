d4c5900f7e575f40df23ca2aea891f78722a6ab9
Atsuya Takagi <atsuya.takagi@gmail.com>
2011-09-24 19:41:32 -0700

    i think i got basic of client side.

--- commit-it.sh
+++ commit-it.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-URL=`cat config`
+COMMITHUB_HOME=${HOME}/.commithub
+
+URL=`cat ${COMMITHUB_HOME}/config`
 REPOSITORY=`git config --get remote.origin.url`
 COMMIT_INFO=`git log -1 --pretty=oneline HEAD`
 FILE_NAME=`date "+%s"`".jpg"
 
-echo ${URL}
-
-cd ${HOME}/.commithub/scratch
+cd ${COMMITHUB_HOME}/scratch
 imagesnap ${FILE_NAME} > /dev/null
 curl -silent -o /dev/null ${URL} -F "picture=@${FILE_NAME};commit=${COMMIT_INFO};repo=${REPOSITORY}"
 rm ${FILE_NAME}
